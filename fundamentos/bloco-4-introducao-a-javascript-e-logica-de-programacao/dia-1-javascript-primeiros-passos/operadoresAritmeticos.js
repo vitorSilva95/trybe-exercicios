d6001186e7fd0d833826a9e0881ed068b1803b31
Vitor Silva <vitor.o.silva95@gmail.com>
2021-06-28 19:00:04 -0300

    Exercicio #1

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/operadoresAritmeticos.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/operadoresAritmeticos.js
@@ -5,8 +5,18 @@ let b = 5;
 // definidas no começo com os valores que serão operados. Faça programas para:
 // Adição (a + b)
 let soma = (a+b);
-console.log("A soma de "+a+" e "+b+" = "+soma)
 // Subtração (a - b)
+let subtracao = (a-b);
 // Multiplicação (a * b)
+let multiplicacao = (a * b);
 // Divisão (a / b)
+let divisao = (a/b);
 // Módulo (a % b)
+let resto = (a%b);
+// Resultados Operações
+
+console.log("A soma de "+a+" e "+b+" = "+soma);
+console.log("A subtração de "+a+" e "+b+" = "+subtracao);
+console.log("A multiplicação de "+a+" e "+b+" = "+multiplicacao);
+console.log("A divisão de "+a+" e "+b+" = "+divisao);
+console.log("O resto de "+a+" e "+b+" = "+resto);
