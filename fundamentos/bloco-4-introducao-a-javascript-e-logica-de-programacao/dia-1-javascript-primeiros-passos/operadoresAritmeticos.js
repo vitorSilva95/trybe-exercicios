958eb12cb9a0c061a77e5a0fe1e0fb361952f2e8
Vitor Silva <vitor.o.silva95@gmail.com>
2021-06-28 18:53:21 -0300

    Iniciando em JS , operadores Aritimeticos

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/operadoresAritmeticos.js
@@ -0,0 +1,12 @@
+// Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b , 
+
+let a = 2;
+let b = 5;
+// definidas no começo com os valores que serão operados. Faça programas para:
+// Adição (a + b)
+let soma = (a+b);
+console.log("A soma de "+a+" e "+b+" = "+soma)
+// Subtração (a - b)
+// Multiplicação (a * b)
+// Divisão (a / b)
+// Módulo (a % b)
